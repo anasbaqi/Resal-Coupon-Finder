29e9a3cb3a4072ada482c7c3c049c8f0295d5c6a
anasbaqi <anasbaqi.sharaf@gmial.com>
2024-05-21 19:01:51 -0400

    Uses categorizes now, much more efficient

--- main.py
+++ main.py
@@ -1,66 +1,109 @@
 #Loading Packages
 import streamlit as st
-import langchain 
 from langchain_community.chat_models import ChatOpenAI
 import pandas as pd
 import numpy as np
+from deep_translator import GoogleTranslator
 
 
-
-#Lets create a dummy class for storing the coupon information for the different brands (i am assuming the Resal API already includes these info)
-class Brand:
-    def __init__(self, name, code, website):
-        self.name = name
+#Class for coupon information, contains: name in en, arabic, coupon code, category it belongs to, description and id for later
+class Coupons:
+    def __init__(self, name_en, name_ar, code, description="", category=None,  api_id=0):
+        self.name_en = name_en
+        self.name_ar = name_ar
         self.code = code
-        self.website = website
+        self.description = description
+        self.category = category if category is not None else []
+        self.api_id = api_id
 
-# List of Brands
-brand_list = [
-    Brand(name="Noon", code="RSL10", website='https://www.noon.com/?sskey=faf368464a154de192157ec07244c763'),
-    Brand(name="Namshi", code="NM295", website="https://www.namshi.com/saudi-ar/"),
-    Brand(name="Ounass", code="OP52", website="https://en-saudi.ounass.com/"),
-    Brand(name="Styli", code="SOM54", website="https://stylishop.com/sa/en?sskey=cfaead66393141a986ca173f860ffa9e"),
-    Brand(name="Lego", code="MM128", website="https://lego.saudiblocks.com/saudi-en/"),
-    Brand(name="GAP", code="RSL", website=""),
-    Brand(name="Victoria Secret", code="PFJYR", website=""),
-    Brand(name="Sun & Sand", code="EP26", website=""),
-    Brand(name="Foot Locker", code="PFYWR", website=""),
-    Brand(name="H&M", code="PFUYV", website=""),
-    Brand(name="American Eagle", code="PFXPB", website=""),
-    Brand(name="Mumzworld", code="OP93", website=""),
-    Brand(name="ToYou", code="RSL", website=""),
-    Brand(name="REDTAG", code="P91", website=""),
-    Brand(name="The Bodyshop", code="PFGZS", website=""),
-    Brand(name="CitrussTv", code="RSL", website=""),
-    Brand(name="UnderArmour", code="CR67", website=""),
-    Brand(name="fordeal", code="OM36", website=""),
-    Brand(name="Brands For Less", code="PF98", website=""),
-    Brand(name="Mikyajy", code="RA", website=""),
-    Brand(name="R&B", code="BB37", website=""),
-    Brand(name="Forever21", code="BB58", website=""),
-    Brand(name="The Luxury Closet", code="LC5", website=""),
-    Brand(name="Homes R us", code="ASM9", website=""),
-    Brand(name="Mothercare", code="PFQDC", website=""),
-    Brand(name="COS", code="PFDJD", website=""),
-    Brand(name="Eyewa", code="RSL", website=""),
-    Brand(name="New Balance", code="PFEXH", website=""),
-    Brand(name="Pottery Barn", code="PFNMT", website=""),
-    Brand(name="West Elm", code="PFNHW", website=""),
-    Brand(name="AlDakheel Oud", code="PL59", website=""),
-    Brand(name="Bloomingdale's", code="RSL", website=""),
-    Brand(name="Boots", code="PFIPC", website=""),
-    Brand(name="Mamas & Papas", code="RSL", website=""),
-    Brand(name="Metro Brazil", code="PF45", website=""),
-    Brand(name="Toys R Us", code="PTAH", website=""),
-    Brand(name="Ted Baker", code="G63", website=""),
-    Brand(name="Lyle & Scott", code="OM605", website=""),
-    Brand(name="Sivvi", code="RSL30", website="")
+    def __str__(self):
+        return f"Partner(name_en={self.name_en}, name_ar={self.name_ar}, code = {self.code}, category = {self.category})"
+
+# List of Coupons
+coupon_data = [
+    {"code": "RSL10", "partner": {"name_en": "noon", "name_ar": "نون"}},
+    {"code": "RSL0", "partner": {"name_en": "namshi", "name_ar": "نمشي"}},
+    {"code": "EP26", "partner": {"name_en": "SSSports", "name_ar": "الشمس والرمال للرياضة"}},
+    {"code": "SOM54", "partner": {"name_en": "Styli", "name_ar": "ستايلي"}},
+    {"code": "PF4", "partner": {"name_en": "Basharacare", "name_ar": "بشرة كير"}},
+    {"code": "G63", "partner": {"name_en": "Ted Baker", "name_ar": "تيد بيكر"}},
+    {"code": "PJZ", "partner": {"name_en": "Nabataty", "name_ar": "متجر نباتاتي"}},
+    {"code": "PTAH", "partner": {"name_en": "Toys R Us", "name_ar": "تويز آر اس"}},
+    {"code": "CR67", "partner": {"name_en": "UnderArmour", "name_ar": "اندرارمر"}},
+    {"code": "PF45", "partner": {"name_en": "Metro Brazil", "name_ar": "مترو برازيل"}},
+    {"code": "BB37", "partner": {"name_en": "R&B", "name_ar": "آر اند بي"}},
+    {"code": "LC5", "partner": {"name_en": "The Luxury Closet", "name_ar": "ذا لاكجري كلوزيت"}},
+    {"code": "OM36", "partner": {"name_en": "fordeal", "name_ar": "فورديل"}},
+    {"code": "RSL", "partner": {"name_en": "Level Shoes", "name_ar": "ليڤيل شوز"}},
+    {"code": "RSL", "partner": {"name_en": "Mamas & Papas", "name_ar": "ماماز اند باباز"}},
+    {"code": "PF98", "partner": {"name_en": "Brands For Less", "name_ar": "براندز فور ليس"}},
+    {"code": "RSL30", "partner": {"name_en": "Sivvi", "name_ar": "سيفي"}},
+    {"code": "P56", "partner": {"name_en": "Ya Hala", "name_ar": "يا هلا"}},
+    {"code": "PFIPC", "partner": {"name_en": "Boots", "name_ar": "بوتس"}},
+    {"code": "PFEXH", "partner": {"name_en": "New Balance", "name_ar": "نيو بالنس"}},
+    {"code": "PFDJD", "partner": {"name_en": "COS", "name_ar": "كوس"}},
+    {"code": "PFQDC", "partner": {"name_en": "Mothercare", "name_ar": "مذركير"}},
+    {"code": "RSL", "partner": {"name_en": "GAP", "name_ar": "جاب"}},
+    {"code": "PFXPB", "partner": {"name_en": "American Eagle", "name_ar": "أميركان ايجل"}},
+    {"code": "RSL", "partner": {"name_en": "Mumzworld", "name_ar": "ممزورلد"}},
+    {"code": "RSL", "partner": {"name_en": "CitrussTv", "name_ar": "سيتروس"}},
+    {"code": "RA", "partner": {"name_en": "Mikyajy", "name_ar": "مكياجي"}},
+    {"code": "BB58", "partner": {"name_en": "Forever21", "name_ar": "فوريفير 21"}},
+    {"code": "PF32", "partner": {"name_en": "Barakat", "name_ar": "بركات"}},
+    {"code": "PF53", "partner": {"name_en": "Store Us", "name_ar": "ستور اص"}},
+    {"code": "MM128", "partner": {"name_en": "Lego", "name_ar": "ليقو"}},
+    {"code": "OM605", "partner": {"name_en": "Lyle & Scott", "name_ar": "لايل اند سكوت"}},
+    {"code": "PL59", "partner": {"name_en": "AlDakheel Oud", "name_ar": "الدخيل للعود"}},
+    {"code": "PFGZS", "partner": {"name_en": "The Bodyshop", "name_ar": "ذي بودي شوب"}},
+    {"code": "RSL", "partner": {"name_en": "Eyewa", "name_ar": "ايوا"}},
+    {"code": "ASM9", "partner": {"name_en": "Homes R us", "name_ar": "هومز ار اس"}},
+    {"code": "PFNMT", "partner": {"name_en": "Pottery Barn", "name_ar": "بوتري بارن"}},
+    {"code": "RSL", "partner": {"name_en": "Bloomingdale's", "name_ar": "بلومينغديلز"}}
 ]
 
 
-#Setting the brand names as a table the user can see:
-brand_data = [{"Name": brand.name, "Code": brand.code} for brand in brand_list]
-brand_df = pd.DataFrame(brand_data)
+#Created a list of categories and categorized all the coupons:
+categories = {
+    "fashion": ["noon", "namshi", "Styli", "Ted Baker", "Metro Brazil", "R&B", "Level Shoes", "Brands For Less", "Sivvi", "GAP", "American Eagle", "CitrussTv", "Forever21", "Lyle & Scott", "Bloomingdale's", "fordeal", "COS", "Eyewa", "Store Us"],
+    "sports": ["SSSports", "UnderArmour", "New Balance", "Store Us", "noon"],
+    "beauty": ["Basharacare", "Boots", "Mikyajy", "The Bodyshop", "AlDakheel Oud", "Store Us", "noon"],
+    "toys": ["Toys R Us", "Lego", "Store Us", "noon"],
+    "baby": ["Mamas & Papas", "Mothercare", "Toys R Us", "Mumzworld", "Store Us", "noon"],
+    "home": ["CitrussTv", "Homes R us", "Pottery Barn", "Nabataty", "The Luxury Closet", "Store Us", "noon"],
+    "mobile": ["Ya Hala"],
+    "grocery":["Barakat"],
+    "technology":["noon", "Store Us"]
+}
+
+#functiion for categorizing all the coupons, API does not have this information so I did it myself
+def categorize(coupon_data, categories):
+    #adding the categories to the class:
+    for item in coupon_data:
+        brand_name = item['partner']['name_en']
+        item['category'] = []
+        
+        for category, brands in categories.items():
+            if brand_name in brands:
+                item['category'].append(category)
+        
+        if not item['category']:
+            item['category'].append('other')
+
+    # Create a list to store Partner instances
+    coupons = []
+
+    # Create Partner instances from the data and add them to the list
+    for coupon in coupon_data:
+        name_en = coupon["partner"]["name_en"]
+        name_ar = coupon["partner"]["name_ar"]
+        code = coupon["code"]
+        category = coupon["category"]
+        partner = Coupons(name_en, name_ar, code, category=category)
+        coupons.append(partner)
+    
+    return coupons
+
+
 
 
 #Using the llm to retrieve the string that contains the name of the product from the prompt question:
@@ -68,7 +111,7 @@ def get_product(prompt_question, llm):
     """Function to extract the product from the prompt question"""
 
     messages = [
-    ("system", "Do not answer the prompt, just output 1 string from the prompt that is the product the prompt user is asking a coupon for."),
+    ("system", "Do not answer the prompt, just output a single string that is the product the user is asking for."),
     ("human", prompt_question),
     ]
 
@@ -76,66 +119,56 @@ def get_product(prompt_question, llm):
 
 
 
-#Function for creating a string with all brand names:
-def get_brand_names(brands):
-    """Tool that returns a string of all the brand names separated by commas."""
-    return ', '.join(brand.name for brand in brands)
+#Function for creating a string with all coupon names:
+def get_categories(categories):
+    """Tool that returns a string of all the categories separated by commas."""
+    return ', '.join(categories.keys())
 
 
-#Function for retrieving relevent brands using llm:
-def get_relevent_brands(prompt_product, brand_names, llm):
-    """Function to find a list of relevent brands based on the product"""
+#Function for retrieving category using llm:
+def get_relevent_category(prompt_product, category_names, llm):
+    """Function to find a list of relevent category based on the product"""
 
     messages = [
-    ("system", "The prompt provides a product and a list of brand names. Search through the brand names and return a list of ones that might sell the product (maximum of 3 brands).If you don't think any of the brands might have the product, then return a empty list"),
-    ("human", f"Product:{prompt_product}, List of brand names: {brand_names}"),
+    ("system", "The prompt provides a product and a list of categories. Just output which category is most relevent based on the product."),
+    ("human", f"Product:{prompt_product}, List of categories: {category_names}"),
     ]
 
     return llm.invoke(messages)
 
 
-# #Function for retrieving brand coupon codes from the brand name:
-# def get_coupons(relevent_brands, brands):
-#   # Split the string into individual brand names
-#   brand_names = [name.strip() for name in relevent_brands.split(',')]
 
-#   # Iterate over the brand names and check if they match any of the brands in the list
-#   for name in brand_names:
-#       for brand in brands:
-#           if name == brand.name:
-#               st.write("Use Coupon Code:", brand.code,  "to get a discout from", brand.name)
-
-def get_coupons(relevant_brands, brand_list, Brand):
+def get_coupons(relevant_category, coupons, Coupons):
     # Split the string into individual brand names
     #brand_names = [name.strip() for name in relevant_brands.split(',')]
 
     # Filter the list of brands based on the relevant brand names
-    
-    final_list = [brand for brand in brand_list if brand.name in relevant_brands]
-    for brand in final_list:
-        st.write("Use coupon code:", brand.code,  "to get a discount from", brand.name)
+    relevent_coupons = [coupon for coupon in coupons if relevant_category in coupon.category]
 
-
+    for coupon in relevent_coupons:
+        st.write("Use coupon code:", coupon.code, "to get a discount from", coupon.name_en)
 
 
 #Calling all the functions:
-def all_comp(llm_retrieve_product, llm_find_brands, brand_list, uq, Brand):
+def all_comp(llm_retrieve_product, llm_find_brands, category_list, uq, Coupons):
+
+    coupons = categorize(coupon_data, category_list)
 
     product_all = get_product(uq, llm_retrieve_product)
-            
+
     product = product_all.content
+    st.write(product)
+    categories = get_categories(category_list)
 
-    brands = get_brand_names(brand_list)
+    relevent_category = get_relevent_category(product, categories, llm_find_brands)
 
-    relevent_brands = get_relevent_brands(product, brands, llm_find_brands)
+    st.write(relevent_category.content)
 
-
-
-    get_coupons(relevent_brands.content, brand_list, Brand)
+    get_coupons(relevent_category.content, coupons, Coupons)
 
     with st.expander("Token Usage"):
         st.write("Tokens used for retriving product (0.2 temp)", product_all.response_metadata['token_usage'])
-        st.write("Tokens used for finding brands (0.8 temp):", relevent_brands.response_metadata['token_usage'])
+        st.write("Tokens used for finding brands (0.8 temp):", relevent_category.response_metadata['token_usage'])
 
 #-----------------------------------------------------------------------------------------------------------------------------------
 
@@ -144,10 +177,6 @@ def all_comp(llm_retrieve_product, llm_find_brands, brand_list, uq, Brand):
 #The title of the UI:
 st.header('Resal Coupon Finder (Using ChatGPT)')
 st.write("A PoC for utilizing AI Agents for Resal's Save More coupon store.")
-st.divider()
-
-st.subheader('Brand names and their coupon codes:')
-st.dataframe(brand_df, hide_index=True, width=700)
 
 st.divider()
 
@@ -165,10 +194,10 @@ api_key = st.text_input("Enter your OpenAI key to get started", type="password")
 
 
 if api_key:
-    llm_retrieve_product = ChatOpenAI(model_name="gpt-4",temperature=0.2, openai_api_key=api_key)
+    llm_retrieve_product = ChatOpenAI(model_name="gpt-4",temperature=0.4, openai_api_key=api_key)
     llm_find_brands = ChatOpenAI(model_name="gpt-4",temperature=0.8, openai_api_key=api_key)
-    st.write("Example: I want coupons to buy a new TV")
+    st.write("Example: I want to buy a new TV")
     user_question = st.text_input(" What are you looking for: ")
 
     if user_question != "":
-        all_comp(llm_retrieve_product, llm_find_brands, brand_list, user_question, Brand)
+        all_comp(llm_retrieve_product, llm_find_brands, categories, user_question, Coupons)
